449ca1b0a33b4508f1838e4ee76bc56b9ea1eb9e
Frazer Clement <frazer.clement@oracle.com>
2024-09-04 14:59:33 +0100

    Bug#37023549 (1/5) Improve NdbApi SPJ scan protocol timeout handling
    Add error insert to SPJ to allow a 'hard' timeout + failure to
    close to be simulated.
    
    Change-Id: Ie2479be2496ff4eb8ebe32547d8a8cb513708d26
    
    Bug#37023549 (2/5) Improve NdbApi SPJ scan protocol timeout handling
    
    Add SPJ scan testing
    
    SPJ scans are added to the test as they have a different implementation
    in the API and so need to be covered separately.
    
    Due to bugs in SPJ, this test causes data node failure without
    further fixes.
    
    Change-Id: I1b7dd00a3bbaaa88aaf689de2ffc617afd041f62
    
    Bug#37023549 (3/5) Improve NdbApi SPJ scan protocol timeout handling
    
    Modify TC to disconnect bad APIs
    
    TC detects in many cases when an API sends a signal which is
    unexpected due the state discovered on an ApiConnectRecord.
    
    Some cases are understood and expected, and others are not,
    resulting in assertion failures in TC -> data node failure.
    
    These can be useful for finding logic errors but can have
    a large impact on users.
    
    This patch modifies TC to handle some of these situations
    differently :
     - Log information about the mismatch
     - Disconnect the API(s) involved
       (API sending the signal, API owning the ConnectRecord)
    
    This will result in :
      - The API(s) being disconnected by all data nodes
      - API failure handling on all data nodes performing cleanup
        of the API's transaction objects, continuing with commit
        or rollback + then releasing them
      - APIs having to reinitialise their connection state to
        the cluster.
    
    API disconnect + reconnect is generally less work, quicker
    and less risky for system availability than a data node
    restart.
    
    This change in functionality is tested to some extent by
    the SPJ timeout scenario in ndb_scan_protocol_timeout, but
    other situations leading to state mismatch may exist.
    
    Change-Id: Id88df14141983b4fef71e74ce275d6b4e26c6c58
    
    Bug#37023549 (4/5) Improve NdbApi SPJ scan protocol timeout handling
    
    On timeout, make SPJ API mark scans as needing close
    
    SPJ API is aligned to the (fixed) behaviour of NdbApi on protocol
    timeout handling - marking scans as needing close at the kernel.
    
    This causes close() to attempt to close the scan, which will
    succeed in cases where the scan has timed out due to LOAD.
    
    Additionally, SPJ API is modified to log when these protocol
    timeout handling paths are used, and what the result is.
    
    The SPJ tests in ndb_scan_protocol_timeout for the 'LOAD' case
    now pass with no leaks, but the BUG case needs a further fix.
    
    Change-Id: I90e4421228f3db9c2de1de7ebb48ae81123abf68
    
    Bug#37023549 (5/5) Improve NdbApi SPJ scan protocol timeout handling
    
    SPJ Timeout Release on Close timeout
    
    In the case where there is a timeout attempting to close an SPJ scan
    at the kernel, this patch sets the ReleaseOnClose variable so that
    the kernel side ApiConnectRecord will not be reused.
    
    This aligns with the normal scan behaviour in the close-failed-timeout
    case.
    
    The ndb_scan_protocol_timeout testcase result is updated with the
    expected results for SPJ scan timeouts due to load + bugs
    
    Change-Id: I04b199759415c3e0d8f6f6546551c0271fac768c

--- storage/ndb/src/kernel/blocks/dbtc/DbtcMain.cpp
+++ storage/ndb/src/kernel/blocks/dbtc/DbtcMain.cpp
@@ -1874,34 +1874,55 @@ void Dbtc::execTCRELEASEREQ(Signal *signal) {
 /****************************************************************************/
 // Error Handling for TCKEYREQ messages
 /****************************************************************************/
-void Dbtc::signalErrorRefuseLab(Signal *signal,
-                                ApiConnectRecordPtr const apiConnectptr) {
+void Dbtc::handleSignalStateProblem(Signal *signal,
+                                    ApiConnectRecordPtr const apiConnectptr,
+                                    NodeId signalNodeId, Uint32 context) {
   ptrGuard(apiConnectptr);
-  if (apiConnectptr.p->apiConnectstate != CS_DISCONNECTED) {
-    jam();
-    apiConnectptr.p->abortState = AS_IDLE;
-    apiConnectptr.p->apiConnectstate = CS_ABORTING;
-  }  // if
-  sendSignalErrorRefuseLab(signal, apiConnectptr);
-}  // Dbtc::signalErrorRefuseLab()
+  g_eventLogger->warning(
+      "TC %u : Signal from node %u in context %u finds ApiConnectRecord %u "
+      "in unexpected state %u",
+      instance(), signalNodeId, context, apiConnectptr.i,
+      apiConnectptr.p->apiConnectstate);
 
-void Dbtc::sendSignalErrorRefuseLab(Signal *signal,
-                                    ApiConnectRecordPtr const apiConnectptr) {
-  ndbassert(false);
-  ptrGuard(apiConnectptr);
+  /* Handle signal sender */
+  /* Data node involved in errors -> crash, API -> disconnect */
+  ndbrequire(getNodeInfo(signalNodeId).getType() == NODE_TYPE_API);
+  g_eventLogger->warning("TC %u : Disconnecting signal sending node %u",
+                         instance(), signalNodeId);
+
+  /* Ask QMGR to eject signal sending API */
+  signal->theData[0] = 900;
+  signal->theData[1] = signalNodeId;
+  sendSignal(QMGR_REF, GSN_DUMP_STATE_ORD, signal, 2, JBA);
+
+  /* Handle record owner */
   if (apiConnectptr.p->apiConnectstate != CS_DISCONNECTED) {
-    jam();
-    /* Force state print */
-    printState(signal, 12, apiConnectptr, true);
-    ndbabort();
-    signal->theData[0] = apiConnectptr.p->ndbapiConnect;
-    signal->theData[1] = signal->theData[ttransid_ptr];
-    signal->theData[2] = signal->theData[ttransid_ptr + 1];
-    signal->theData[3] = ZSIGNAL_ERROR;
-    sendSignal(apiConnectptr.p->ndbapiBlockref, GSN_TCROLLBACKREP, signal, 4,
-               JBB);
+    const NodeId acrOwnerNodeId = refToNode(apiConnectptr.p->ndbapiBlockref);
+
+    if (acrOwnerNodeId == signalNodeId) {
+      jam();
+      return;
+    }
+
+    g_eventLogger->warning(
+        "TC %u : ApiConnectRecord %u receiving signal in unexpected state %u "
+        "from node %u owned by different node %u",
+        instance(), apiConnectptr.i, apiConnectptr.p->apiConnectstate,
+        signalNodeId, acrOwnerNodeId);
+
+    /* Data node errors -> crash, API -> disconnect */
+    ndbrequire(getNodeInfo(acrOwnerNodeId).getType() == NODE_TYPE_API);
+    g_eventLogger->warning("TC %u : Disconnecting record owning node %u",
+                           instance(), acrOwnerNodeId);
+
+    /* Ask QMGR to eject API */
+    signal->theData[0] = 900;
+    signal->theData[1] = acrOwnerNodeId;
+    sendSignal(QMGR_REF, GSN_DUMP_STATE_ORD, signal, 2, JBA);
   }
-}  // Dbtc::sendSignalErrorRefuseLab()
+
+  /* Do nothing more - API disconnection is responsible for cleanup */
+}  // Dbtc::handleSignalStateProblem
 
 void Dbtc::abortBeginErrorLab(Signal *signal,
                               ApiConnectRecordPtr const apiConnectptr) {
@@ -1926,7 +1947,7 @@ void Dbtc::printState(Signal *signal, int place,
   if (!force_trace) return;
 
   g_eventLogger->info(
-      "-- Dbtc::printState -- "
+      "-- Dbtc::printState(%u) -- "
       "Received from place = %d"
       " apiConnectptr.i = %u"
       " apiConnectstate = %d "
@@ -1946,8 +1967,8 @@ void Dbtc::printState(Signal *signal, int place,
       "tckeyrec = %u"
       " returnsignal = %d"
       " apiFailState = %u",
-      place, apiConnectptr.i, apiConnectptr.p->apiConnectstate, ctcTimer,
-      apiConnectptr.p->ndbapiBlockref, apiConnectptr.p->transid[0],
+      instance(), place, apiConnectptr.i, apiConnectptr.p->apiConnectstate,
+      ctcTimer, apiConnectptr.p->ndbapiBlockref, apiConnectptr.p->transid[0],
       apiConnectptr.p->transid[1], getApiConTimer(apiConnectptr),
       apiConnectptr.p->counter, apiConnectptr.p->lqhkeyconfrec,
       apiConnectptr.p->lqhkeyreqrec, apiConnectptr.p->cascading_scans_count,
@@ -1996,8 +2017,7 @@ void Dbtc::TCKEY_abort(Signal *signal, int place,
       return;
     case 1:
       jam();
-      printState(signal, 3, apiConnectptr);
-      sendSignalErrorRefuseLab(signal, apiConnectptr);
+      ndbabort();  // Not used currently
       return;
     case 2: {
       printState(signal, 6, apiConnectptr);
@@ -2266,8 +2286,7 @@ void Dbtc::TCKEY_abort(Signal *signal, int place,
 
     case 55:
       jam();
-      printState(signal, 5, apiConnectptr);
-      sendSignalErrorRefuseLab(signal, apiConnectptr);
+      ndbabort();  // Not used currently
       return;
 
     case 56: {
@@ -2426,12 +2445,8 @@ void Dbtc::execKEYINFO(Signal *signal) {
       return; /* IGNORE */
     case CS_CONNECTED:
       jam();
-      /****************************************************************>*/
-      /*       MOST LIKELY CAUSED BY A MISSED SIGNAL. SEND REFUSE AND   */
-      /*       SET STATE TO ABORTING.                                   */
-      /****************************************************************>*/
-      printState(signal, 11, apiConnectptr);
-      signalErrorRefuseLab(signal, apiConnectptr);
+      handleSignalStateProblem(signal, apiConnectptr,
+                               refToNode(signal->getSendersBlockRef()), 1);
       return;
     case CS_STARTED:
       jam();
@@ -3104,7 +3119,8 @@ void Dbtc::execTCKEYREQ(Signal *signal) {
         compare_transid1 = compare_transid1 | compare_transid2;
         if (unlikely(compare_transid1 != 0)) {
           releaseSections(handle);
-          TCKEY_abort(signal, 1, apiConnectptr);
+          handleSignalStateProblem(signal, apiConnectptr,
+                                   refToNode(sendersBlockRef), 2);
           return;
         }  // if
         ndbrequire(regApiPtr->apiCopyRecord != RNIL);
@@ -3188,7 +3204,8 @@ void Dbtc::execTCKEYREQ(Signal *signal) {
        * THUS THERE IS NO ACTION FROM THE API THAT CAN SPEED UP THIS PROCESS.
        *---------------------------------------------------------------------*/
       releaseSections(handle);
-      TCKEY_abort(signal, 55, apiConnectptr);
+      handleSignalStateProblem(signal, apiConnectptr,
+                               refToNode(sendersBlockRef), 3);
       return;
   }  // switch
 
