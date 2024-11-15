422fdd958a5217130f4ae3b4bfa23d5908e830f2
Tor Didriksen <tor.didriksen@oracle.com>
2024-11-15 12:59:18 +0100

    Bug#37273525 libc++ is deprecating/removing char_traits<unsigned char> [noclose]
    Implement the missing parts from patch one, i.e. types/functions needed for
    std::basic_ostringstream<unsigned char, ....>
    
    Add unit tests for some of the functions.
    
    Change-Id: Id93229a8796dd5685d1251f7d4c63c36cbb4b779

--- include/my_char_traits.h
+++ include/my_char_traits.h
@@ -24,7 +24,10 @@
 #ifndef MY_CHAR_TRAITS_INCLUDED
 #define MY_CHAR_TRAITS_INCLUDED
 
+#include <compare>
+#include <cstdio>
 #include <cstring>
+#include <iosfwd>
 
 template <class CharT>
 struct my_char_traits;
@@ -33,32 +36,66 @@ struct my_char_traits;
   This is a standards-compliant, drop-in replacement for
     std::char_traits<unsigned char>
   We need this because clang libc++ is removing support for it in clang 19.
-  It is not a complete implementation. Rather we implement just enough to
-  compile any usage of char_traits<uchar> we have in our codebase.
  */
 template <>
 struct my_char_traits<unsigned char> {
   using char_type = unsigned char;
   using int_type = unsigned int;
+  using off_type = std::streamoff;
+  using pos_type = std::streampos;
+  using standards = std::mbstate_t;
+  using comparison_category = std::strong_ordering;
 
-  static void assign(char_type &c1, const char_type &c2) { c1 = c2; }
-
-  static char_type *assign(char_type *s, std::size_t n, char_type a) {
-    return static_cast<char_type *>(memset(s, a, n));
+  static constexpr void assign(char_type &c1, const char_type &c2) noexcept {
+    c1 = c2;
+  }
+  static constexpr bool eq(char_type c1, char_type c2) noexcept {
+    return c1 == c2;
+  }
+  static constexpr bool lt(char_type c1, char_type c2) noexcept {
+    return c1 < c2;
   }
 
-  static int compare(const char_type *s1, const char_type *s2, std::size_t n) {
+  static int compare(const char_type *s1, const char_type *s2,
+                     std::size_t n) noexcept {
+    if (n == 0) return 0;
     return memcmp(s1, s2, n);
   }
-
-  static char_type *move(char_type *s1, const char_type *s2, std::size_t n) {
+  static size_t length(const char_type *s) noexcept {
+    return strlen(static_cast<const char *>(static_cast<const void *>(s)));
+  }
+  static const char_type *find(const char_type *s, size_t n,
+                               const char_type &a) noexcept {
+    if (n == 0) return nullptr;
+    return static_cast<const char_type *>(memchr(s, a, n));
+  }
+  static char_type *move(char_type *s1, const char_type *s2,
+                         std::size_t n) noexcept {
     if (n == 0) return s1;
     return static_cast<char_type *>(memmove(s1, s2, n));
   }
-
-  static char_type *copy(char_type *s1, const char_type *s2, std::size_t n) {
+  static char_type *copy(char_type *s1, const char_type *s2,
+                         std::size_t n) noexcept {
     if (n == 0) return s1;
     return static_cast<char_type *>(memcpy(s1, s2, n));
+  }
+  static char_type *assign(char_type *s, std::size_t n, char_type a) noexcept {
+    if (n == 0) return s;
+    return static_cast<char_type *>(memset(s, a, n));
+  }
+
+  static constexpr int_type not_eof(int_type c) noexcept {
+    return eq_int_type(c, eof()) ? ~eof() : c;
+  }
+  static constexpr char_type to_char_type(int_type c) noexcept {
+    return static_cast<char_type>(c);
+  }
+  static constexpr int_type to_int_type(char_type c) noexcept { return c; }
+  static constexpr bool eq_int_type(int_type c1, int_type c2) noexcept {
+    return c1 == c2;
+  }
+  static constexpr int_type eof() noexcept {
+    return static_cast<int_type>(EOF);
   }
 };
 
